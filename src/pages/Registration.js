1139d8f2f14cd797073231825bcf4493bdbbc738
Gerardo Montero <gerardomontme@gmail.com>
2021-11-08 16:58:01 -0800

    used yup validation

--- src/pages/Registration.js
+++ src/pages/Registration.js
@@ -1,79 +1,75 @@
 import React, { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
-import { Formik, Form, Field, ErrorMessage } from 'formik';
+import { Formik, Form, Field } from 'formik';
+import * as Yup from 'yup';
+import "./Registration.scss";
+
+const SignupSchema = Yup.object().shape({
+  username: Yup.string()
+    .min(5, "Username must be at least 5 characters long")
+    .max(16, "Username can't be more than 16 characters long")
+    .required('Required'),
+  email: Yup.string().email('Invalid email').required('Required'),
+  password: Yup.string()
+    .required('Required') 
+    .min(8, 'Password must be must be at least 8 characters long'),
+  confirmPassword: Yup.string()
+    .oneOf([Yup.ref('password'), null], 'Passwords must match')
+});
 
 const Registration = () => {
-
-  const [credentials, setCredentials] = useState(undefined);
-
 
   // This will get socket from redux store now we can call our server
   // but we will not close our connection on navigation
   const socket = useSelector((state) => state.socket.socket);
-  useEffect(() => {
-    if (credentials) {
-      // emits register event to server
-      socket.emit("register", { 
-        username: credentials.username, 
-        email: credentials.email, 
-        password: credentials.password,
-        confirmPassword: credentials.confirmPassword
-      });
 
-      // listens for registration result from server
-      socket.on("registration_result", (result) => {
-        console.log("registration result", result);
-      });
-      // eslint-disable-next-line react-hooks/exhaustive-deps
-    }
-  }, [credentials]);
+  const registerUser = (credentials) => {
+    // emits register event to server
+    socket.emit("register", { 
+      username: credentials.username, 
+      email: credentials.email, 
+      password: credentials.password,
+      confirmPassword: credentials.confirmPassword
+    });
+
+    // listens for registration result from server
+    socket.on("registration_result", (result) => {
+      console.log("registration result", result);
+    });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }
   return (
     <>
       <h1>Registration</h1>
-      <Formik
-       initialValues={{ username: '', email: '', password: '', confirmPassword: '' }}
-       validate={values => {
-         const errors = {};
-         if (!values.username) {
-          errors.username = 'Required';
-         } else if (!values.email) {
-           errors.email = 'Required';
-         } else if (!values.password) {
-          errors.password = 'Required';
-         } else if (values.password.length<8) {
-          errors.password = 'Password must be at least 8 characters long';
-         } else if (!values.confirmPassword) {
-          errors.confirmPassword = 'Required';
-         } else if (
-           !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
-         ) {
-           errors.email = 'Invalid email address';
-         } else if (values.confirmPassword !== values.password) {
-           errors.confirmPassword = 'Passwords do not match'
-         }
-         return errors;
-       }}
-       onSubmit={(values, { setSubmitting }) => {
-           setCredentials(values);
-           setSubmitting(false);
-       }}
-      >
-       {({ isSubmitting }) => (
-         <Form>
-           <Field name="username" placeholder="Enter username"/>
-           <ErrorMessage name="username" component="div" />
-           <Field type="email" name="email" placeholder="Enter email"/>
-           <ErrorMessage name="email" component="div" />
-           <Field type="password" name="password" placeholder="Enter password"/>
-           <ErrorMessage name="password" component="div" />
-           <Field type="password" name="confirmPassword" placeholder="Confirm password"/>
-           <ErrorMessage name="confirmPassword" component="div" />
-           <button type="submit" disabled={isSubmitting}>
-             Submit
-           </button>
-         </Form>
-       )}
-     </Formik>
+      <div className="registrationContainer">
+        <Formik
+          initialValues={{ username: '', email: '', password: '', confirmPassword: '' }}
+          validationSchema={SignupSchema}
+          onSubmit={values =>  registerUser(values)}
+        >
+        {({ errors, touched }) => (
+          <Form className="registrationForm">
+            <div>
+              <Field name="username" placeholder="Enter username"/>
+              {errors.username && touched.username ? ( <span>{errors.username}</span> ) : null}
+            </div>
+            <div>
+              <Field type="email" name="email" placeholder="Enter email"/>
+              {errors.email && touched.email ? ( <span>{errors.email}</span> ) : null}
+            </div>
+            <div>
+              <Field type="password" name="password" placeholder="Enter password"/>
+              {errors.password && touched.password ? ( <span>{errors.password}</span> ) : null}
+            </div>
+            <div>
+              <Field type="password" name="confirmPassword" placeholder="Confirm password"/>
+              {errors.confirmPassword && touched.confirmPassword ? ( <span>{errors.confirmPassword}</span> ) : null}
+            </div>
+            <button type="submit">Submit</button>
+          </Form>
+        )}
+      </Formik>
+      </div>
     </>
   );
 };
