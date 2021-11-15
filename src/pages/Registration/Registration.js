c15b2872fecf0977abf3390b6b8788c9b9664046
Ondrej Belza <belzaondra@gmail.com>
2021-11-15 14:44:46 -0800

    add layout components (header and footer)

--- src/pages/Registration/Registration.js
+++ src/pages/Registration/Registration.js
@@ -1,37 +1,42 @@
 import { Field, Form, Formik } from "formik";
-import React, { useState } from "react";
+import React from "react";
 import { Col, Container, Row } from "react-bootstrap";
 import { useSelector } from "react-redux";
 import * as Yup from "yup";
-import Logo from "../icons/logo.svg";
+import Logo from "../../icons/logo.svg";
+import "./Registration.scss";
 
 const SignupSchema = Yup.object().shape({
+  username: Yup.string()
+    .min(5, "Username must be at least 5 characters long")
+    .max(16, "Username can't be more than 16 characters long")
+    .required("Required"),
   email: Yup.string().email("Invalid email").required("Required"),
-  password: Yup.string().required("Required"),
+  password: Yup.string()
+    .required("Required")
+    .min(8, "Password must be must be at least 8 characters long"),
+  confirmPassword: Yup.string()
+    .oneOf([Yup.ref("password"), null], "Passwords must match")
+    .required("Required"),
 });
 
-const Login = () => {
-  const [message, setMessage] = useState("");
-
+const Registration = () => {
   // This will get socket from redux store now we can call our server
   // but we will not close our connection on navigation
   const socket = useSelector((state) => state.socket.socket);
 
-  const LoginUser = (credentials) => {
+  const registerUser = (credentials) => {
     // emits register event to server
-    socket.emit("login", {
+    socket.emit("register", {
+      username: credentials.username,
       email: credentials.email,
       password: credentials.password,
+      confirmPassword: credentials.confirmPassword,
     });
 
-    // listens for login result from server
-    socket.on("login_result", (result) => {
-      console.log("login result", result);
-      if (!result) {
-        setMessage("Invalid credentials");
-      } else {
-        alert("Logged in!");
-      }
+    // listens for registration result from server
+    socket.on("registration_result", (result) => {
+      console.log("registration result", result);
     });
     // eslint-disable-next-line react-hooks/exhaustive-deps
   };
@@ -45,13 +50,23 @@ const Login = () => {
           </Col>
           <Col className="columns">
             <div className="registrationContainer">
-              <p>{message}</p>
               <Formik
-                initialValues={{ email: "", password: "" }}
+                initialValues={{
+                  username: "",
+                  email: "",
+                  password: "",
+                  confirmPassword: "",
+                }}
                 validationSchema={SignupSchema}
-                onSubmit={(values) => LoginUser(values)}>
+                onSubmit={(values) => registerUser(values)}>
                 {({ errors, touched }) => (
                   <Form className="registrationForm">
+                    <Field name="username" placeholder="Enter username" />
+                    {errors.username && touched.username ? (
+                      <span>{errors.username}</span>
+                    ) : (
+                      <span></span>
+                    )}
                     <Field
                       type="email"
                       name="email"
@@ -72,7 +87,17 @@ const Login = () => {
                     ) : (
                       <span></span>
                     )}
-                    <button type="submit">Login</button>
+                    <Field
+                      type="password"
+                      name="confirmPassword"
+                      placeholder="Confirm password"
+                    />
+                    {errors.confirmPassword && touched.confirmPassword ? (
+                      <span>{errors.confirmPassword}</span>
+                    ) : (
+                      <span></span>
+                    )}
+                    <button type="submit">Register</button>
                   </Form>
                 )}
               </Formik>
@@ -84,4 +109,4 @@ const Login = () => {
   );
 };
 
-export default Login;
+export default Registration;
