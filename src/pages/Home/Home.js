6404c53d0006d183ef7bfd86c2338957b67888e8
BlackStone8960 <fng.trf.kk1020@gmail.com>
2021-11-16 15:31:42 -0800

    created home page

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -2,15 +2,32 @@ import './Home.scss';
 
 const Home = () => {
   return (
-    <div className="home-wrapper">
-      <div className="twin-container">
-        <div className="join-table-box">
-          <h1>Join Table</h1>
-          <div>Stakes</div>
-          <div className="stake-box">
-            <div className="stake-inside-flex">
-              <div>1k</div>
-              <div className="chip-wrapper"></div>
+    <div className="home-container">
+      <div className="home-wrapper">
+        <div className="twin-container">
+          <div className="home-box" id="join-table">
+            <div className="home-box-container">
+              <h1 className="home-box-header">Join Table</h1>
+              <div className="stakes-header">Stakes</div>
+              <div className="stake-box">
+                <div className="stake-inside-flex">
+                  <div>1k</div>
+                  <div className="chip-wrapper"></div>
+                </div>
+                <div className="stake-inside-flex">
+                  <div>10k</div>
+                  <div className="chip-wrapper"></div>
+                </div>
+                <div className="stake-inside-flex">
+                  <div>100k</div>
+                  <div className="chip-wrapper"></div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="home-box" id="item-shop">
+            <div className="home-box-container">
+              <h1 className="home-box-header">Item Shop</h1>
             </div>
           </div>
         </div>
