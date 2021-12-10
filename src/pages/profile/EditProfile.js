55cca17d96e26e8cc4531922f3de4e8e836f4516
BlackStone8960 <fng.trf.kk1020@gmail.com>
2021-12-09 17:21:57 -0800

    created edit profile page

--- src/pages/profile/EditProfile.js
+++ src/pages/profile/EditProfile.js
@@ -1,24 +1,126 @@
 import { useSelector } from "react-redux";
-import "./Profile.scss";
+import * as Yup from "yup";
+import { Field, Form, Formik } from "formik";
+import Client from "../../utils/axiosClient";
+import { useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import getUser from "../../utils/getUser";
+import { setUser } from "../../redux/user/userSlice";
+import "./EditProfile.scss";
+
+const EditProfileSchema = Yup.object().shape({
+  name: Yup.string().required("Required"),
+  email: Yup.string().email("Invalid email").required("Required"),
+  currentPassword: Yup.string().required("Required"),
+  newPassword: Yup.string().required("Required"),
+});
 
 const EditProfile = () => {
   const { user } = useSelector((state) => state.user);
+  const dispatch = useDispatch();
+  let navigate = useNavigate();
+
+  const editUser = async (credentials) => {
+    try {
+      await Client.post("edit_profile", credentials);
+
+      //loads user data
+      const user = await getUser();
+      dispatch(setUser(user));
+
+      //navigates to home
+      navigate("/profile");
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
   return (
-    <div className="profile-container">
-      <div className="profile-wrapper">
-        <h1 className="profile-header">Profile</h1>
-        <div className="profile-list-wrapper">
-          <div className="profile-list">
-            <div className="profile-list-item">Name</div>
-            <div className="profile-list-item">{user.username}</div>
-          </div>
-          <div className="profile-list profile-list--lastone">
-            <div className="profile-list-item">Email</div>
-            <div className="profile-list-item">{user.email}</div>
-          </div>
+    <div className="profile-edit-container">
+      <div className="profile-edit-wrapper">
+        <h1 className="profile-edit-header">Profile</h1>
+        <div className="profile-edit-form-wrapper">
+          <Formik
+            initialValues={{
+              name: user.username,
+              email: user.email,
+              currentPassword: "",
+              newPassword: "",
+            }}
+            validationSchema={EditProfileSchema}
+            onSubmit={(values) => EditProfile(values)}
+          >
+            {({ errors, touched }) => (
+              <Form className="profile-edit-form">
+                <label htmlFor="name" className="profile-edit-form-label">
+                  Name
+                </label>
+                <Field
+                  type="text"
+                  name="name"
+                  id="name"
+                  placeholder="Enter your name"
+                />
+                {errors.name && touched.name ? (
+                  <span>{errors.name}</span>
+                ) : (
+                  <span></span>
+                )}
+                <label htmlFor="email" className="profile-edit-form-label">
+                  Email
+                </label>
+                <Field
+                  type="email"
+                  name="email"
+                  id="email"
+                  placeholder="Enter email"
+                />
+                {errors.email && touched.email ? (
+                  <span>{errors.email}</span>
+                ) : (
+                  <span></span>
+                )}
+                <label
+                  htmlFor="currentPassword"
+                  className="profile-edit-form-label"
+                >
+                  Current Password
+                </label>
+                <Field
+                  type="password"
+                  name="currentPassword"
+                  placeholder="Enter current password"
+                  id="currentPassword"
+                />
+                {errors.currentPassword && touched.currentPassword ? (
+                  <span>{errors.currentPassword}</span>
+                ) : (
+                  <span></span>
+                )}
+                <label
+                  htmlFor="newPassword"
+                  className="profile-edit-form-label"
+                >
+                  New Password
+                </label>
+                <Field
+                  type="password"
+                  name="newPassword"
+                  placeholder="Enter new password"
+                  id="newPassword"
+                />
+                {errors.newPassword && touched.newPassword ? (
+                  <span>{errors.newPassword}</span>
+                ) : (
+                  <span></span>
+                )}
+                <button type="submit" className="save-button">
+                  Save
+                </button>
+              </Form>
+            )}
+          </Formik>
         </div>
-        <div className="save-button">Save</div>
       </div>
     </div>
   );
